8caa68c6e1112791715754d548fe9799ffe16395
Aaron Shaver <aaronshaverpdx@gmail.com>
2022-05-22 08:48:57 -0700

    realized we can simply use string for UUID, no need for anything fancier

--- app/user.py
+++ app/user.py
@@ -2,14 +2,13 @@ import uuid
 import json
 from uuid import UUID
 from .points import Points
-from .encoder_helpers import UUIDEncoder
 
 
 class User:
 
     def __init__(self):
-        self.user_id = uuid.uuid4()
+        self.user_id = str(uuid.uuid4())  # don't need static typing of UUID
         self.points = Points()
 
     def __str__(self):
-        return json.dumps(self.__dict__, cls=UUIDEncoder)
+        return json.dumps(self.__dict__)
